327864304040bac0a95c946a6856505c70e33276
Gabriel Araujo <gabrielcac7@gmail.com>
2025-11-10 01:56:57 -0300

    Criei os migrations

---
+++ src/ASI.MiniModelagem/Migrations/20251110045534_InitialCreate.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ASI.MiniModelagem.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Sigla = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projetos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projetos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Professores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    DepartamentoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Professores_Departamentos_DepartamentoId",
+                        column: x => x.DepartamentoId,
+                        principalTable: "Departamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProfessoresProjetos",
+                columns: table => new
+                {
+                    ProfessorId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProjetoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProfessoresProjetos", x => new { x.ProfessorId, x.ProjetoId });
+                    table.ForeignKey(
+                        name: "FK_ProfessoresProjetos_Professores_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProfessoresProjetos_Projetos_ProjetoId",
+                        column: x => x.ProjetoId,
+                        principalTable: "Projetos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departamentos",
+                columns: new[] { "Id", "Nome", "Sigla" },
+                values: new object[,]
+                {
+                    { 1, "Departamento de Engenharias e Computação", "DEC" },
+                    { 2, "Departamento de Ciências Exatas", "DCEX" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Projetos",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "Projeto ASI" },
+                    { 2, "Projeto math" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Professores",
+                columns: new[] { "Id", "DepartamentoId", "Nome" },
+                values: new object[,]
+                {
+                    { 1, 1, "Larissa" },
+                    { 2, 2, "Gabriel" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProfessoresProjetos",
+                columns: new[] { "ProfessorId", "ProjetoId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Professores_DepartamentoId",
+                table: "Professores",
+                column: "DepartamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfessoresProjetos_ProjetoId",
+                table: "ProfessoresProjetos",
+                column: "ProjetoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProfessoresProjetos");
+
+            migrationBuilder.DropTable(
+                name: "Professores");
+
+            migrationBuilder.DropTable(
+                name: "Projetos");
+
+            migrationBuilder.DropTable(
+                name: "Departamentos");
+        }
+    }
+}
